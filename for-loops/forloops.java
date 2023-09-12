cf74c184e93b906a157bc30197121e3c2fd6125c
Jasper Gough <jasper.gough@mrjaffesclass.com>
2023-09-12 11:32:31 -0700

    for loops done

---
+++ for-loops/forloops.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class forloops here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class forloops
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        for (int i = 0; i < 15; i += 1) {
+            System.out.println("i is equal to " +i);
+        }
+        for (int j = 10; j > 0 || j < 0; j -= 2) {
+            System.out.println("j is equal to " +j);
+        }
+        for (int k = 10; k != 0; k -= 3) {
+            System.out.println("k is equal to " +k);
+        }
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        forloops hw = new forloops();
+        hw.run();
+    }
+}
+
